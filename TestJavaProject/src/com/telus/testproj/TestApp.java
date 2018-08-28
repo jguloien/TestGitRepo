2009605e5c23f57c2f42662c255e23ab641b6dc9
Jim Guloien <Jim.Guloien@TELUS.com>
2018-08-28 09:36:50 -0600

    Initial check-in of project

---
+++ TestJavaProject/src/com/telus/testproj/TestApp.java
@@ -0,0 +1,10 @@
+package com.telus.testproj;
+
+public class TestApp
+{
+    public static void main(String[] args)
+    {
+    	// Write message:
+        System.out.println("Hello, World");
+    }
+}
