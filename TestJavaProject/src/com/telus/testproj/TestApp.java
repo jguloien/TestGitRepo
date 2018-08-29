1d7b197817b56aaf9c7ca6b4333d6316519ea6f8
Jim Guloien <Jim.Guloien@TELUS.com>
2018-08-29 09:36:51 -0600

    Documented the TestApp

--- TestJavaProject/src/com/telus/testproj/TestApp.java
+++ TestJavaProject/src/com/telus/testproj/TestApp.java
@@ -1,17 +1,24 @@
 package com.telus.testproj;
-
+/*
+ * TestApp
+ */
 public class TestApp
 {
     public static void main(String[] args)
     {
+    	// Say hi
         System.out.println("Hello World");
         
+        // Ask how they are?
         System.out.println("How are you World?");
         
+        // Answer for them
         System.out.println("You are fine World?");
         
+        // Say good-bye
         System.out.println("Goodbye World");
         
+        // The End:
         System.out.println("This is the end World");
     }
 }
